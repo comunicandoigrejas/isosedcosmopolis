0f31e1a13ee5598be88aaedae9838f4d31facfec
Comunicando Igrejas <comunicandoigrejas@gmail.com>
2026-02-14 17:16:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -141,12 +141,12 @@ elif st.session_state.pagina == "Agenda":
         df['data'] = pd.to_datetime(df['data'], dayfirst=True, errors='coerce')
         df = df.dropna(subset=['data']) # Remove linhas sem data
 
-        def conectar_planilha():
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+def conectar_planilha():
+        scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     # Puxa a chave dos Secrets do Streamlit Cloud
-    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
-    client = gspread.authorize(creds)
-    return client.open_by_url(URL_PLANILHA)
+        creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
+        client = gspread.authorize(creds)
+        return client.open_by_url(URL_PLANILHA)
 
 def salvar_novo_usuario(lista_dados):
     try:
